0f5331747a4005ffe6ed2276b1aaeaec33245016
Kris Jenkins <krisajenkins@gmail.com>
2014-02-25 14:04:46 +0000

    Updating the dependency list.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject crash-course-clojure "1.0.0"
   :description "A Crash Course In Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.7"]
-                 [cheshire "5.2.0"]])
+                 [clj-http "0.7.9"]
+                 [cheshire "5.3.1"]])
 
